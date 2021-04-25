740f8c23555eb34fe3c47ee65e0ebdb20467991e
Administrator <liuwenjing089@163.com>
2021-04-25 23:37:04 +0800

    创建分支dev

--- cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
+++ cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
@@ -5,5 +5,7 @@ public class GitLearn {
         System.out.println("更新1");
         System.out.println("更新2");
         System.out.println("更新3");
+        System.out.println("创建分支dev");
+
     }
 }
