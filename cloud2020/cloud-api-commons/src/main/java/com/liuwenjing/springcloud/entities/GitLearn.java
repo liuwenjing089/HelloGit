8dd1cb6419775f3f29c4b33205895fd1a74514ec
Administrator <liuwenjing089@163.com>
2021-04-25 23:23:02 +0800

    更新2

--- cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
+++ cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
@@ -3,5 +3,6 @@ package com.liuwenjing.springcloud.entities;
 public class GitLearn {
     public static void main(String[] args) {
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
