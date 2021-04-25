bc20748f3dba98079c62e278ec42ba180f59baa0
Administrator <liuwenjing089@163.com>
2021-04-25 23:51:47 +0800

    主干master添加

--- cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
+++ cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
@@ -4,6 +4,7 @@ public class GitLearn {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("将dev合并到master");
+        System.out.println("主干master添加");
 
 
     }
