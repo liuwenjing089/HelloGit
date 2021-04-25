77a0d555c2080bc53dd4f92807fe8103fe4d7014
Administrator <liuwenjing089@163.com>
2021-04-25 23:46:56 +0800

    将dev合并到master

--- cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
+++ cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
@@ -3,9 +3,8 @@ package com.liuwenjing.springcloud.entities;
 public class GitLearn {
     public static void main(String[] args) {
         System.out.println("更新1");
-        System.out.println("更新2");
-        System.out.println("更新3");
-        System.out.println("创建分支dev");
+        System.out.println("将dev合并到master");
+
 
     }
 }
