13cb5f7c4bf77467d83d187ff5f363fc901760ba
Administrator <liuwenjing089@163.com>
2021-04-28 23:23:28 +0800

    本地dev

--- cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
+++ cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
@@ -6,7 +6,7 @@ public class GitLearn {
         System.out.println("将dev合并到master");
         System.out.println("主干master添加");
         System.out.println("分支dev添加");
-        System.out.println("远程分支master再次更新");
+        System.out.println("本地dev:liuwening-dev");
 
 
     }
