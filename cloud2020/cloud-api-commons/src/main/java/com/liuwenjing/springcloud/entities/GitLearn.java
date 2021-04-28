c69bd3064a8dfcc6ba32c0815dcf3599317698b6
liuwenjing089 <60388831+liuwenjing089@users.noreply.github.com>
2021-04-29 01:01:08 +0800

    Update GitLearn.java
    NN

--- cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
+++ cloud2020/cloud-api-commons/src/main/java/com/liuwenjing/springcloud/entities/GitLearn.java
@@ -10,7 +10,8 @@ public class GitLearn {
         System.out.println("本地dev:local-liuwenjing-dev冲突,与远程master再次冲突");
         System.out.println("远程分支master,与本地dev:local-liuwenjing-dev冲突--SEVEN");
         System.out.println("远程分支master,与本地dev:local-liuwenjing-dev冲突--LL");
-        System.out.println("远程分支master,与本地dev:local-liuwenjing-dev冲突--NINE");
+        System.out.println("远程分支master,与本地dev:local-liuwenjing-dev冲突--NN");
+        System.out.println("远程分支master,与本地dev:local-liuwenjing-dev冲突--MM");
 
 
     }
